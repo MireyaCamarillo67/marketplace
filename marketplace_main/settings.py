7ac6cfce7a35eca209846e687402ab0b46affa00
MireyaCamarillo67 <m28542437@gmail.com>
2026-05-26 00:27:38 -0700

    agrege models y admin en store

--- marketplace_main/settings.py
+++ marketplace_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'marketplace_main.urls'
+AUTH_USER_MODEL = 'store.User'
 
 TEMPLATES = [
     {
