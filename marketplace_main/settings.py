a73fdfe6f5d99247d8ec9e160f89ee07cf8209fe
MireyaCamarillo67 <m28542437@gmail.com>
2026-05-26 00:59:32 -0700

    Sprint 5

--- marketplace_main/settings.py
+++ marketplace_main/settings.py
@@ -115,8 +115,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = 'static/'
+import os
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+ 
+LOGIN_URL = 'login'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
